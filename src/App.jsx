45a1639a3330659acb872e75cfb1676bf6dcce4b
abu sayed <mdabusayedbinamin@gmail.com>
2025-12-25 18:37:52 +0600

    add update and edit feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,70 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
+
 
 const App = () => {
 
-  const [list, setList] = useState([]);
+  const [list,setList] = useState([]);
+  const [item,setItem] = useState("");
+  const [edit , setEdit] = useState(null);
 
-  const [item, setItem] = useState("");
 
-  const addTodo = () => {
-    if (item.trim() === "") return;
-
-    setList([...list, item]);
-    setItem("");
-  };
-  const removeTodo = (index) =>{
-    list.splice(index, 1)
+  const addTodo = ()=>{
+    if(item.trim() === "") return;
+    list.push(item);
+    setList([...list])
+    setItem("")
+  }
+  const removeTodo = (index)=>{
+    list.splice(index,1);
     setList([...list])
   }
+  const startEdit = (index)=>{
+    setItem(list[index])
+    setEdit(index)
+  }
+  const updateTodo = ()=>{
+    list[edit] = item;
+    setList([...list]);
+    setEdit(null);
+    setItem("");
+  }
+
+
+
+
 
   return (
     <div>
-      <h2>Todo App</h2>
-
-      <input
-        type="text"
-        placeholder="Enter Something"
-        value={item}
-        onChange={(e) => setItem(e.target.value)}
-      />
-
-      <button onClick={addTodo}>Add</button>
+      <h2>Todo List</h2>
+      <input type="text" value={item} onChange={(e) =>{setItem(e.target.value)}} />
+      {
+        edit !== null? (
+          <button onClick={updateTodo}>Update</button>
+        ) : (
+         <button onClick={addTodo}>Add Todo</button>
+        )
+      }
 
       <table>
         <tbody>
-          {list.length !== 0 ? (
-            list.map((element, index) => (
-              <tr key={index}>
-                <td>{element}</td>
-                <td><button onClick={()=> {removeTodo(index)}} > Remove</button></td>
+          {
+            list.length !== 0 ? (
+              list.map((element , index) =>(
+                <tr key={index}>
+                  <td>{element}</td>
+                  <td><button onClick={()=>{removeTodo(index)}}>Remove</button>
+                  <button onClick={()=>{startEdit(index)}}> Edit</button>
+                  
+                   </td>
+                </tr>
+              ))
+            ): (
+              <tr>
+                <td>No Data Found</td>
               </tr>
-            ))
-          ) : (
-            <tr>
-              <td>No data</td>
-            </tr>
-          )}
+            )
+          }
         </tbody>
       </table>
     </div>
